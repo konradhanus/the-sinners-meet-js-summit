05829eab16c27658acde838e3d79cee11b713624
Konrad Hanus <khanus@workforcesoftware.com>
2022-09-14 13:04:51 +0200

    Add Animation loop

--- src/components/Engine/animateLoop.js
+++ src/components/Engine/animateLoop.js
@@ -4,6 +4,8 @@ function animateLoop({background, platforms, player}){
     background && background.draw();
     drawPlatforms(platforms);
     player && player.draw();
+
+    requestAnimationFrame(() => animateLoop({background, platforms, player}))
 }
 
 export default animateLoop;
